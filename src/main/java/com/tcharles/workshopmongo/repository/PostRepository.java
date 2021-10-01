a9f3436a1991082904c9bd8eada54224eeab2a70
tcharles w. barreto <tcharlesigt@gmail.com>
2021-10-01 14:13:41 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/tcharles/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.tcharles.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tcharles.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
