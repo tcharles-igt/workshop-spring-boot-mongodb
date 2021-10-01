114869b2820d7c0c7744b0c22cfffe1f3ca3d9d0
tcharles w. barreto <tcharlesigt@gmail.com>
2021-10-01 15:32:43 -0300

    Obtendo um post por id

---
+++ src/main/java/com/tcharles/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.tcharles.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tcharles.workshopmongo.domain.Post;
+import com.tcharles.workshopmongo.repository.PostRepository;
+import com.tcharles.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
